05344fe740e1c809a469744fba3d795bb292777f
Abraham Fatoki <fatokiabraham@gmail.com>
2023-07-22 04:02:30 +0100

    add report and summary modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module, ClassSerializerInterceptor } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SummaryModule } from './summary/summary.module';
+import { ServiceController } from './service/service.controller';
+import { ReportModule } from './report/report.module';
 
 @Module({
-   imports: [],
-   controllers: [AppController],
+   imports: [SummaryModule, ReportModule],
+   controllers: [AppController, ServiceController],
    providers: [
       AppService,
       {
